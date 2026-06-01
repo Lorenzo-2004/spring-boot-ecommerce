5337f75363baabb47bf9a9de397d1ca9be8b1c4b
Lorenzo-2004 <lorenzorafanomezantsoa@gmail.com>
2026-06-01 12:55:10 +0300

    updating webconfig

--- backend/src/main/java/com/ecommerce/backend/config/WebConfig.java
+++ backend/src/main/java/com/ecommerce/backend/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.ecommerce.backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +12,17 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "https://spring-boot-ecommerce-nine.vercel.app",
+                        "http://localhost:3000"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
